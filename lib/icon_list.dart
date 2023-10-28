817d64524d9dffdd8101a90a55d2b6d3c35dec83
Magno_Daza <magnodaza1@gmail.com>
2023-10-28 05:17:41 +0000

    Iconlist funcionando

--- lib/icon_list.dart
+++ lib/icon_list.dart
@@ -1,101 +1,84 @@
-//archivo icon_list.dart
-// Importa el paquete necesario para usar los widgets de Flutter
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
-// Crea una clase que extienda de StatelessWidget
-class IconList extends StatelessWidget {
-  // Cambia la lista de IconData que contiene los iconos de Cupertino por una lista de IconData que contenga los iconos de Material que quieras mostrar
-  final List<IconData> iconList = [
-    Icons.favorite,
-    CupertinoIcons.add_circled,
-    CupertinoIcons.heart_fill,
-    CupertinoIcons.bell,
-    CupertinoIcons.airplane,
-    CupertinoIcons.alarm,
-    CupertinoIcons.ant_circle_fill,
-    Icons.favorite_border,
-    Icons.favorite_outline,
-    Icons.favorite_rounded,
-    Icons.favorite_sharp,
-    Icons.favorite,
-    Icons.favorite_border,
-    Icons.favorite_outline,
-    Icons.favorite_rounded,
-    Icons.favorite_sharp,
-    Icons.favorite,
-    Icons.favorite_border,
-    Icons.favorite_outline,
-    Icons.favorite_rounded,
-    Icons.favorite_sharp,
-    Icons.favorite,
-    Icons.favorite_border,
-    Icons.favorite_outline,
-    Icons.favorite_rounded,
-    Icons.favorite_sharp,
-    Icons.favorite,
-    Icons.favorite_border,
-    Icons.favorite_outline,
-    Icons.favorite_rounded,
-    Icons.favorite_sharp,
-    Icons.favorite,
-    Icons.favorite_border,
-    Icons.favorite_outline,
-    Icons.favorite_rounded,
-    Icons.favorite_sharp,
-    Icons.favorite,
-    Icons.favorite_border,
-    Icons.favorite_outline,
-    Icons.favorite_rounded,
-    Icons.favorite_sharp,
-    // Añade más iconos según tus preferencias
-  ];
+class IconList extends StatefulWidget {
+  @override
+  _IconListState createState() => _IconListState();
+}
+
+class _IconListState extends State<IconList> {
+  final int _iconsPerPage = 25;
+  final PageController _pageController = PageController();
+  int _currentPage = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController.addListener(() {
+      setState(() {
+        _currentPage = _pageController.page!.round();
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    // Calcula el número de páginas
-    int numPages = (iconList.length / 25).ceil();
+    final int _totalPages = (icons.length / _iconsPerPage).ceil();
 
-    // Divide la lista de iconos en páginas
-    List<List<IconData>> iconPages = [];
-    for (int i = 0; i < numPages; i++) {
-      int start = i * 25;
-      int end = start + 25;
-      if (end > iconList.length) end = iconList.length;
-      iconPages.add(iconList.sublist(start, end));
-    }
-
-    // Devuelve el widget que quieres extraer
-    return Container(
-      // Asigna un valor al alto del contenedor
-      height: 300,
-      // Usa un widget PageView para permitir deslizar entre las páginas de iconos
-      child: PageView.builder(
-        itemCount: numPages,
-        itemBuilder: (context, pageIndex) {
-          return Column(
-            children: [
-              Expanded(
-                child: GridView.count(
-                  crossAxisCount: 5,
-                  children: iconPages[pageIndex].map((icon) {
-                    return InkWell(
-                      child: Icon(icon),
-                      onTap: () {
-                        print(icon);
-                        Navigator.of(context).pop();
+    return Center(
+      child: Column(
+        children: <Widget>[
+          SizedBox(height: 10),
+          Text('Selecciona un icono',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+          SizedBox(height: 10),
+          Container(
+            height: MediaQuery.of(context).size.height /
+                3, // Ajusta esto según tus necesidades
+            child: PageView.builder(
+              controller: _pageController,
+              itemCount: _totalPages,
+              itemBuilder: (BuildContext context, int page) {
+                return GridView.builder(
+                  itemCount: (page < _totalPages - 1)
+                      ? _iconsPerPage
+                      : icons.length % _iconsPerPage,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 5,
+                  ),
+                  itemBuilder: (BuildContext context, int index) {
+                    int iconIndex = page * _iconsPerPage + index;
+                    return IconButton(
+                      icon: Icon(icons[iconIndex]),
+                      onPressed: () {
+                        Navigator.pop(context, icons[iconIndex]);
                       },
                     );
-                  }).toList(),
-                ),
-              ),
-              Text(
-                'Página ${pageIndex + 1} de $numPages',
-              ),
-            ],
-          );
-        },
+                  },
+                );
+              },
+            ),
+          ),
+          Text('Página ${_currentPage + 1} de $_totalPages'),
+        ],
       ),
     );
   }
 }
+
+const List<IconData> icons = [
+  Icons.ac_unit,
+  Icons.access_alarm,
+  Icons.favorite,
+  CupertinoIcons.add_circled,
+  CupertinoIcons.heart_fill,
+  CupertinoIcons.bell,
+  CupertinoIcons.airplane,
+  CupertinoIcons.alarm,
+  CupertinoIcons.ant_circle_fill,
+  Icons.favorite_border,
+  Icons.favorite_outline,
+  Icons.favorite_rounded,
+  Icons.favorite_sharp,
+  Icons.favorite_border_outlined, // Añade más íconos aquí
+];
