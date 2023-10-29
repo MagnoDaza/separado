467ba2026744d273a8c5aae49e17a75f8c17ded5
Magno_Daza <magnodaza1@gmail.com>
2023-10-29 03:10:12 +0000

    ahora es mas agraba organizar los tabs

--- lib/tab_organizer_page.dart
+++ lib/tab_organizer_page.dart
@@ -1,4 +1,3 @@
-// archivo: tab_organizer_page.dart
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:provider/provider.dart';
@@ -11,43 +10,47 @@ class TabOrganizerPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Organizar los tabs'),
       ),
-      body: ReorderableListView(
-        onReorder: (oldIndex, newIndex) =>
-            Provider.of<TabProvider>(context, listen: false)
-                .reorderTabs(oldIndex, newIndex),
-        children: Provider.of<TabProvider>(context).myTabs.map((tabData) {
-          return ListTile(
-            key: Key(tabData.text),
-            leading: Icon(tabData.icon),
-            title: Text(tabData.text),
-          );
-        }).toList(),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton:
+      body: Column(
+        children: [
+          Expanded(
+            child: ReorderableListView(
+              onReorder: (oldIndex, newIndex) =>
+                  Provider.of<TabProvider>(context, listen: false)
+                      .reorderTabs(oldIndex, newIndex),
+              children: Provider.of<TabProvider>(context).myTabs.map((tabData) {
+                return ListTile(
+                  key: Key(tabData.text),
+                  leading: Icon(tabData.icon),
+                  title: Text(tabData.text),
+                );
+              }).toList(),
+            ),
+          ),
           Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-        FloatingActionButton.extended(
-          onPressed: () {
-            // Aquí puedes implementar la lógica para cancelar los cambios
-            Navigator.pop(context);
-          },
-          label: Text('Cancelar'),
-          icon: Icon(Icons.cancel),
-        ),
-        FloatingActionButton.extended(
-          onPressed: () {
-            // Aquí puedes implementar la lógica para confirmar los cambios
-            Fluttertoast.showToast(
-              msg: "Cambios guardados",
-              toastLength: Toast.LENGTH_SHORT,
-              gravity: ToastGravity.BOTTOM,
-            );
-            Navigator.pop(context);
-          },
-          label: Text('Confirmar'),
-          icon: Icon(Icons.check),
-        ),
-      ]),
+            FloatingActionButton.extended(
+              onPressed: () {
+                // Aquí puedes implementar la lógica para cancelar los cambios
+                Navigator.pop(context);
+              },
+              label: Text('Cancelar'),
+              icon: Icon(Icons.cancel),
+            ),
+            FloatingActionButton.extended(
+              onPressed: () {
+                // Aquí puedes implementar la lógica para confirmar los cambios
+                Fluttertoast.showToast(
+                  msg: "Cambios guardados",
+                  toastLength: Toast.LENGTH_SHORT,
+                  gravity: ToastGravity.BOTTOM,
+                );
+                Navigator.pop(context);
+              },
+              label: Text('Confirmar'),
+              icon: Icon(Icons.check),
+            ),
+          ]),
+        ],
+      ),
     );
   }
 }
