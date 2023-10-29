058c3df91eab8f905a36b612840ad0ee4e443069
Magno_Daza <magnodaza1@gmail.com>
2023-10-29 03:46:49 +0000

    aun falta la implementación de referesh p

--- lib/tab_organizer_page.dart
+++ lib/tab_organizer_page.dart
@@ -2,54 +2,81 @@ import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:provider/provider.dart';
 import 'tab_provider.dart';
+import 'tab_data.dart';
 
-class TabOrganizerPage extends StatelessWidget {
+class TabOrganizerPage extends StatefulWidget {
+  @override
+  _TabOrganizerPageState createState() => _TabOrganizerPageState();
+}
+
+class _TabOrganizerPageState extends State<TabOrganizerPage> {
+  late List<TabData> workingTabs;
+
+  @override
+  void initState() {
+    super.initState();
+    workingTabs =
+        List.from(Provider.of<TabProvider>(context, listen: false).myTabs);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Organizar los tabs'),
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            child: ReorderableListView(
-              onReorder: (oldIndex, newIndex) =>
-                  Provider.of<TabProvider>(context, listen: false)
-                      .reorderTabs(oldIndex, newIndex),
-              children: Provider.of<TabProvider>(context).myTabs.map((tabData) {
-                return ListTile(
-                  key: Key(tabData.text),
-                  leading: Icon(tabData.icon),
-                  title: Text(tabData.text),
-                );
-              }).toList(),
+    return WillPopScope(
+      onWillPop: () async {
+        return true;
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Organizar los tabs'),
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              child: ReorderableListView(
+                onReorder: (oldIndex, newIndex) {
+                  setState(() {
+                    if (newIndex > oldIndex) {
+                      newIndex -= 1;
+                    }
+                    final TabData item = workingTabs.removeAt(oldIndex);
+                    workingTabs.insert(newIndex, item);
+                  });
+                },
+                children: workingTabs.map((tabData) {
+                  return ListTile(
+                    key: Key(tabData.text),
+                    leading: Icon(tabData.icon),
+                    title: Text(tabData.text),
+                  );
+                }).toList(),
+              ),
             ),
-          ),
-          Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-            FloatingActionButton.extended(
-              onPressed: () {
-                // Aquí puedes implementar la lógica para cancelar los cambios
-                Navigator.pop(context);
-              },
-              label: Text('Cancelar'),
-              icon: Icon(Icons.cancel),
-            ),
-            FloatingActionButton.extended(
-              onPressed: () {
-                // Aquí puedes implementar la lógica para confirmar los cambios
-                Fluttertoast.showToast(
-                  msg: "Cambios guardados",
-                  toastLength: Toast.LENGTH_SHORT,
-                  gravity: ToastGravity.BOTTOM,
-                );
-                Navigator.pop(context);
-              },
-              label: Text('Confirmar'),
-              icon: Icon(Icons.check),
-            ),
-          ]),
-        ],
+            SizedBox(height: 15),
+            Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+              FloatingActionButton.extended(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                label: Text('Cancelar'),
+                icon: Icon(Icons.cancel),
+              ),
+              FloatingActionButton.extended(
+                onPressed: () {
+                  Provider.of<TabProvider>(context, listen: false).myTabs =
+                      workingTabs;
+                  Fluttertoast.showToast(
+                    msg: "Cambios guardados",
+                    toastLength: Toast.LENGTH_SHORT,
+                    gravity: ToastGravity.BOTTOM,
+                  );
+                  Navigator.pop(context);
+                },
+                label: Text('Confirmar'),
+                icon: Icon(Icons.check),
+              ),
+            ]),
+          ],
+        ),
       ),
     );
   }
