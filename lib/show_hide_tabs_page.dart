dfe5797cb664a427340ec7e37e05a4d8aad4640a
Magno_Daza <magnodaza1@gmail.com>
2023-11-03 22:07:34 +0000

    Ahora ya no existe el arhcibo showhideswitch. El  codigo  ahora aparece completo de showhidetabpage

--- lib/show_hide_tabs_page.dart
+++ lib/show_hide_tabs_page.dart
@@ -2,7 +2,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'tab_provider.dart';
-import 'show_hide_switch.dart'; // Asegúrate de que esta ruta de importación es correcta
 import 'package:fluttertoast/fluttertoast.dart';
 
 class ShowHideTabsPage extends StatelessWidget {
@@ -51,6 +50,35 @@ class ShowHideTabsPage extends StatelessWidget {
           ),
         ),
       ),
+    );
+  }
+}
+
+class ShowHideSwitch extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<TabProvider>(
+      builder: (context, tabProvider, child) {
+        return SwitchListTile(
+          title: Text(tabProvider.showText
+              ? 'Se muestran todos los nombres de los tabs'
+              : 'Se muestran todos los iconos'),
+          value: tabProvider.showText,
+          onChanged: tabProvider.customNamesEnabled
+              ? null
+              : (bool value) {
+                  tabProvider.toggleShowText();
+                  tabProvider.toggleShowIcons();
+                  Fluttertoast.showToast(
+                    msg: tabProvider.showText
+                        ? "Nombre mostrado"
+                        : "Icono mostrado",
+                    toastLength: Toast.LENGTH_SHORT,
+                    gravity: ToastGravity.BOTTOM,
+                  );
+                },
+        );
+      },
     );
   }
 }
